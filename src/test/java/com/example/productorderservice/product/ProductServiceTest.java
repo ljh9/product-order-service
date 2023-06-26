09e3b2ee7941a22ae0d8ab08066b6204c846dbef
ljh9 <dkssud2208@naver.com>
2023-06-26 23:44:21 +0900

    update service

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,10 +1,17 @@
 package com.example.productorderservice.product;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.server.MockWebSession;
 import org.springframework.util.Assert;
 
 public class ProductServiceTest {
     private ProducService productService;
+
+    @BeforeEach
+    void setUp() {
+        productService = new ProducService();
+    }
 
     @Test
     void add() {
@@ -16,8 +23,12 @@ public class ProductServiceTest {
     }
 
     private class ProducService {
+        private ProductPort productPort;
+
         public void addProduct(final AddProductRequest request) {
-            throw new UnsupportedOperationException("");
+            final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+
+            productPort.save(product);
         }
     }
     
@@ -34,4 +45,22 @@ public class ProductServiceTest {
     }
 
 
+    private class Product {
+        private final String name;
+        private final int price;
+        private final DiscountPolicy discountPolicy;
+
+        public Product(final String name, int price, DiscountPolicy discountPolicy) {
+            Assert.hasText(name, "상품명 필수");
+            Assert.isTrue(price > 0, "0보다크게");
+            Assert.notNull(discountPolicy, "할인필수");
+            this.name = name;
+            this.price = price;
+            this.discountPolicy = discountPolicy;
+        }
+    }
+
+    private interface ProductPort {
+        void save(final Product product);
+    }
 }
