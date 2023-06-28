f58527a39f2a92dcd7e0bfc7e894117e99d9750c
ljh9 <dkssud2208@naver.com>
2023-06-28 21:32:33 +0900

    update service

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -4,18 +4,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductServiceTest {
     private ProductService productService;
     private ProductPort productPort;
+    private ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
-        productPort = new ProductPort() {
-            @Override
-            public void save(final Product product) {
-
-            }
-        };
+//        productPort = new ProductPort() {
+//            @Override
+//            public void save(final Product product) {
+//
+//            }
+//        };
+        productRepository = new ProductRepository();
+        productPort = new ProductAdapter(productRepository);
         productService = new ProductService(productPort);
     }
 
@@ -56,9 +62,12 @@ public class ProductServiceTest {
 
 
     private class Product {
+
+        private Long id;
         private final String name;
         private final int price;
         private final DiscountPolicy discountPolicy;
+
 
         public Product(final String name, int price, DiscountPolicy discountPolicy) {
             Assert.hasText(name, "상품명 필수");
@@ -68,6 +77,14 @@ public class ProductServiceTest {
             this.price = price;
             this.discountPolicy = discountPolicy;
         }
+
+        public void assignId(final Long aLong) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
     }
 
     private interface ProductPort {
@@ -75,9 +92,27 @@ public class ProductServiceTest {
     }
 
     private class ProductAdapter implements ProductPort {
+        private final ProductRepository productRepository;
+
+        private ProductAdapter(final ProductRepository productRepository) {
+            this.productRepository = productRepository;
+        }
+
         @Override
         public void save(final Product product){
+            productRepository.save(product);
+        }
+    }
 
+    private class ProductRepository {
+
+        private Map<Long, Product> persistence = new HashMap<>();
+        private Long sequence = 0L;
+
+
+        public void save(final Product product) {
+            product.assignId(++sequence);
+            persistence.put(product.getId(), product);
         }
     }
 }
