f1dad37be5dce647e57732dfd035619ffd362304
ljh9 <dkssud2208@naver.com>
2023-07-13 22:55:38 +0900

    update test

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,9 +1,13 @@
 package com.example.productorderservice.product;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
 
     @Test
